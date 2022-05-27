e763cd6763444c2441b99c3f2c66562b76440e29
BDODINKA <BDODINKA@users.noreply.github.com>
2022-05-27 20:44:32 +0300

    feat:add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,1 +1,20 @@
-// import React from 'react';
+import React from 'react';
+import {Wrapper} from "../Wrapper/Wrapper";
+import styles from "../../styleComponents/footer/footer.module.css"
+
+
+export const Footer = (props: any) => {
+    if (props.value === 'footer') {
+        return (
+            <div className={styles.footer}>
+                <Wrapper value={1}/>
+            </div>
+        );
+    }
+    return (
+        <div className={styles.footer}>
+            ERROR
+        </div>
+    );
+
+}
